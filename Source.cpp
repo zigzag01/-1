d0c8164f91d5a7ada419a3adf1e1aa353705e0da
zigzag01 <91686105+zigzag01@users.noreply.github.com>
2021-10-05 17:11:05 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+//Белоушко Елизавета П43021
+{
+	//Программа вычисления стоимости покупки.
+	int y1, y2;
+	double cymma, x1, x2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	cymma = (x1 * y1) + (x2 * y2);
+	cout << cymma << endl;
+
+	return 0;
+}
